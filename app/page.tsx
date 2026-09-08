8554ec9b1dfcc2b323ec172736cb9e7c12ff3348
BaysalIrl <baysal.h@outlook.com>
2026-09-08 09:30:03 +0000

    AEK ve Besiktas logolari duzeltildi

--- app/page.tsx
+++ app/page.tsx
@@ -45,7 +45,7 @@ const TEAM_LOGOS: Record<string, string> = {
   // Türk Takımları
   'Galatasaray': 'https://crests.football-data.org/610.png',
   'Fenerbahçe': 'https://crests.football-data.org/613.png',
-  'Beşiktaş': 'https://crests.football-data.org/611.png',
+  'Beşiktaş': 'Beşiktaş': 'https://crests.football-data.org/600.png',
 
   // İspanya
   'Real Madrid': 'https://crests.football-data.org/86.png',
@@ -99,7 +99,7 @@ const TEAM_LOGOS: Record<string, string> = {
   'Slovan Bratislava': 'https://crests.football-data.org/2144.png',
   'Slovan': 'https://crests.football-data.org/2144.png',
   'AEK Athens': 'https://crests.football-data.org/600.png',
-  'AEK': 'https://crests.football-data.org/600.png',
+  'AEK': 'https://upload.wikimedia.org/wikipedia/en/thumb/3/36/AEK_Athens_FC_logo.svg/300px-AEK_Athens_FC_logo.svg.png',
   'Viking': 'https://crests.football-data.org/335.png',
   'Sabah': 'https://crests.football-data.org/8468.png',
 };
